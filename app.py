daf80626a9289b7881ad1d41922c2c094c5899b4
Vignesh C <vigneshc@wekancode.com>
2018-07-05 00:01:57 +0530

    name change for alexa decorator

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
 app.config['ASK_APPLICATION_ID'] = AppConstants.ALEXA_SKILL_ID
 logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 assist = Assistant(app, route='/')
-ask = Ask(app, '/')
 
 kural = Thirukural()
 
@@ -48,37 +47,39 @@ def thirukuralDetail():
     return ask(speech)
 
 # Amazon Alexa
-@ask.launch
+alexa = Ask(app, '/')
+
+@alexa.launch
 def launched():
     text = render_template('welcome')
     return question(text)
 
-@ask.intent('AMAZON.HelpIntent')
+@alexa.intent('AMAZON.HelpIntent')
 def help():
     text = render_template('help')
     return question(text)
 
-@ask.intent('AMAZON.CancelIntent')
-@ask.intent('AMAZON.StopIntent')
+@alexa.intent('AMAZON.CancelIntent')
+@alexa.intent('AMAZON.StopIntent')
 def stop():
     text = render_template('cancel')
     return statement(text)
 
-@ask.intent('thirukural')
+@alexa.intent('thirukural')
 def thirukuralAlexaIntent():
     text = kural.getThirukural()
     speech = text['Translation'] + ". Do you want to know the meaning of that or read the next."
     session.attributes['kural'] = json.dumps(text)
     return question(speech)
 
-@ask.intent('next')
+@alexa.intent('next')
 def thirukuralNextAlexaIntent():
     text = kural.getThirukural()
     speech = text['Translation'] + ". Do you want to know the meaning of that or read the next."
     session.attributes['kural'] = json.dumps(text)
     return question(speech)
 
-@ask.intent('detail')
+@alexa.intent('detail')
 def thirukuralDetailAlexa():
     if 'kural' in session.attributes:
         kural = session.attributes['kural']
