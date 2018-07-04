5fe8ed135baaaa908d912dee8dcd555eb60cad61
Vignesh C <vigneshc@wekancode.com>
2018-07-04 11:00:01 +0530

    alexa skill sdk and functions added

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from flask_assistant import Assistant, tell, ask
 from flask_assistant import context_manager
+from flask_ask import Ask, statement, question, session
 from thirukural import Thirukural
+from constants import AppConstants
 import logging
 import json
 
 app = Flask(__name__)
 app.config['ASSIST_ACTIONS_ON_GOOGLE'] = True
+app.config['ASK_APPLICATION_ID'] = AppConstants.ALEXA_SKILL_ID
 logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 assist = Assistant(app, route='/')
+ask = Ask(app, '/')
 
 kural = Thirukural()
 
+@app.route('/')
+def index():
+    return 'welcome to Thirukural.'
+
+# Google Assistant
 @assist.action('welcome')
 def hello_world():
     speech = 'Welcome to thirukural, You can ask for a thirukural'
@@ -37,6 +46,47 @@ def thirukuralDetail():
     kural = json.loads(kural)
     speech = kural['explanation']
     return ask(speech)
+
+# Amazon Alexa
+@ask.launch
+def launched():
+    text = render_template('welcome')
+    return question(text)
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    text = render_template('help')
+    return question(text)
+
+@ask.intent('AMAZON.CancelIntent')
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    text = render_template('cancel')
+    return statement(text)
+
+@ask.intent('thirukural')
+def thirukuralAlexaIntent():
+    text = kural.getThirukural()
+    speech = text['Translation'] + ". Do you want to know the meaning of that or read the next."
+    session.attributes['kural'] = json.dumps(text)
+    return question(speech)
+
+@ask.intent('next')
+def thirukuralNextAlexaIntent():
+    text = kural.getThirukural()
+    speech = text['Translation'] + ". Do you want to know the meaning of that or read the next."
+    session.attributes['kural'] = json.dumps(text)
+    return question(speech)
+
+@ask.intent('detail')
+def thirukuralDetailAlexa():
+    if 'kural' in session.attributes:
+        kural = session.attributes['kural']
+        kural = json.loads(kural)
+        speech = kural['explanation']
+    else:
+        speech = 'You must ask a kural to know about the detail'
+    return question(speech)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
 class AppConstants:
     JSON_FILE_NAME = "thirukkural.json"
+    ALEXA_SKILL_ID = "amzn1.ask.skill.0eb29e12-32aa-4ff5-b51d-6be03b75255f"
